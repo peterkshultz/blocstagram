24e984663a99a456c3cb8001e727abdf5587773b
Peter Shultz <peterkshultz@verizon.net>
2014-11-26 10:45:33 -0500

    Initial Commit

---
+++ Blocstagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blocstagram
+//
+//  Created by Peter Shultz on 11/26/14.
+//  Copyright (c) 2014 Peter Shultz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Blocstagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Blocstagram
+//
+//  Created by Peter Shultz on 11/26/14.
+//  Copyright (c) 2014 Peter Shultz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
